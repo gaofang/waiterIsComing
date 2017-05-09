c2b76b7fd015bcae0da93f5462b25b6500d4bb4e
gaofang <837682676@qq.com>
2017-05-09 20:54:25 +0800

    buju

--- pages/menu/menu.js
+++ pages/menu/menu.js
@@ -4,10 +4,6 @@ Page({
     logs: []
   },
   onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(function (log) {
-        return util.formatTime(new Date(log))
-      })
-    })
+    
   }
 })

7b7de55e49f892bc47ac0127f068b6a26d9866db
gaofang <837682676@qq.com>
2017-05-10 21:00:21 +0800

    yangshi

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,24 +3,115 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    menu: [
+      {
+        cuisineName: '主食',
+        list: [
+          {
+            name: '韭菜猪肉饺子',
+            price: 14
+          },
+          {
+            name: '白菜猪肉饺子',
+            price: 15
+          },
+          {
+            name: '香菇猪肉饺子',
+            price: 16
+          },
+          {
+            name: '牛肉猪肉饺子',
+            price: 17
+          },
+          {
+            name: '羊肉猪肉饺子',
+            price: 18
+          }
+        ]
+      },
+      {
+        cuisineName: '肉菜',
+        list: [
+          {
+            name: '肉菜1',
+            price: 14
+          },
+          {
+            name: '肉菜2',
+            price: 15
+          },
+          {
+            name: '肉菜3',
+            price: 16
+          },
+          {
+            name: '肉菜4',
+            price: 17
+          },
+          {
+            name: '肉菜5',
+            price: 18
+          },
+          {
+            name: '肉菜6',
+            price: 19
+          },
+          {
+            name: '肉菜1',
+            price: 20
+          }
+        ]
+      },
+      {
+        cuisineName: '素菜',
+        list: [
+          {
+            name: '这是一盘素菜1',
+            price: 14
+          },
+          {
+            name: '这是一盘素菜1',
+            price: 15
+          },
+          {
+            name: '这是一盘素菜1',
+            price: 16
+          },
+          {
+            name: '这是一盘素菜1',
+            price: 17
+          },
+          {
+            name: '这是一盘素菜1',
+            price: 18
+          },
+          {
+            name: '这是一盘素菜1',
+            price: 19
+          },
+          {
+            name: '这是一盘素菜1',
+            price: 20
+          }
+        ]
+      }
+    ],
+    subMenu: null,
+    activeIdx: 0
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  chooseClass: function(e) {
+    this.setData({
+      subMenu: this.data.menu[e.currentTarget.dataset.index].list,
+      activeIdx: e.currentTarget.dataset.index
     })
   },
   onLoad: function () {
-    console.log('onLoad')
+    console.log(this.data.menu[0].list);
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
+    this.setData({
+      subMenu: this.data.menu[0].list
     })
   }
 })
